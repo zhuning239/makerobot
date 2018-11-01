42b689ee7c08ccda03c561c30be09d43ce0e4cfd
zhuning239 <364711106@qq.com>
2018-11-01 19:06:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ load dependency
 
 
 //% color="#31C7D5" weight=10 icon="\uf1d0"
-namespace robotbit {
+namespace makerobot {
     const PCA9685_ADDRESS = 0x40
     const MODE1 = 0x00
     const MODE2 = 0x01
